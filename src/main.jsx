b6c3c82674a4fd95cb86da432a1ba081aee06bcd
Jon Green <jongreen1996@gmail.com>
2023-03-17 14:57:15 +0000

    Installed and setup empty store using redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</BrowserRouter>
 	</React.StrictMode>
 )
