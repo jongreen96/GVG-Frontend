2c7927281d342ea6ca235a3db63f06bd5bac1f91
Jon Green <jongreen1996@gmail.com>
2023-03-20 13:35:59 +0000

    Installed Axios and updated authSlice to save user

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,9 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 const checkLoginStatus = createAsyncThunk('auth/checkLoginStatus', async () => {
 	try {
-		const response = await authAPI.isLoggedIn()
-		return response.ok
+		const response = await axios.get('http://localhost:3000/auth')
+		return response.data
 	} catch (err) {
 		throw err
 	}
@@ -12,14 +13,24 @@ const checkLoginStatus = createAsyncThunk('auth/checkLoginStatus', async () => {
 const authSlice = createSlice({
 	name: 'auth',
 	initialState: {
-		isAuthenticated: false,
+		user: null,
+		status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+		error: null,
 	},
 	reducers: {},
 	extraReducers: (builder) => {
-		builder.addCase(checkLoginStatus.fulfilled, (state, action) => {
-			state.isAuthenticated = action.payload
-			console.log('authSlice.js: state.isAuthenticated = action.payload', state.isAuthenticated)
-		})
+		builder
+			.addCase(checkLoginStatus.pending, (state, action) => {
+				state.status = 'loading'
+			})
+			.addCase(checkLoginStatus.fulfilled, (state, action) => {
+				state.status = 'succeeded'
+				state.user = action.payload.user
+			})
+			.addCase(checkLoginStatus.rejected, (state, action) => {
+				state.status = 'failed'
+				state.error = action.message
+			})
 	},
 })
 
