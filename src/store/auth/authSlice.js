714eb11cc7f43bab58044eb74474245eb42c8be3
Jon Green <jongreen1996@gmail.com>
2023-03-24 18:04:55 +0000

    Fixed login persistance caused by axios

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,43 +1,49 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const checkLoginStatus = createAsyncThunk('auth/checkLoginStatus', async () => {
 	try {
 		const response = await axios.get('http://localhost:3000/auth', {
 			withCredentials: true,
-		})
-		// console.log(response.data)
-		return response.data
+		});
+		return response.data;
 	} catch (err) {
-		throw err
+		throw err;
 	}
-})
+});
 
 export const login = createAsyncThunk('auth/login', async (credentials) => {
 	try {
-		const response = await axios.post('http://localhost:3000/login', credentials)
-		return response.data.user
+		const response = await fetch('http://localhost:3000/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(credentials),
+			credentials: 'include',
+		});
+		return response.data.user;
 	} catch (err) {
-		throw err
+		throw err;
 	}
-})
+});
 
 export const logout = createAsyncThunk('auth/logout', async () => {
 	try {
-		const response = await axios.post('http://localhost:3000/logout')
+		const response = await axios.post('http://localhost:3000/logout');
 	} catch (err) {
-		throw err
+		throw err;
 	}
-})
+});
 
 export const register = createAsyncThunk('auth/register', async (credentials) => {
 	try {
-		const response = await axios.post('http://localhost:3000/register', credentials)
-		return response.data
+		const response = await axios.post('http://localhost:3000/register', credentials);
+		return response.data;
 	} catch (err) {
-		throw err
+		throw err;
 	}
-})
+});
 
 const authSlice = createSlice({
 	name: 'auth',
@@ -50,30 +56,30 @@ const authSlice = createSlice({
 	extraReducers: (builder) => {
 		builder
 			.addCase(checkLoginStatus.pending, (state, action) => {
-				state.status = 'loading'
+				state.status = 'loading';
 			})
 			.addCase(checkLoginStatus.fulfilled, (state, action) => {
-				state.status = 'succeeded'
-				state.user = action.payload
+				state.status = 'succeeded';
+				state.user = action.payload;
 			})
 			.addCase(checkLoginStatus.rejected, (state, action) => {
-				state.status = 'failed'
-				state.error = action.message
+				state.status = 'failed';
+				state.error = action.message;
 			})
 			.addCase(login.pending, (state, action) => {
-				state.status = 'loading'
+				state.status = 'loading';
 			})
 			.addCase(login.fulfilled, (state, action) => {
-				state.status = 'succeeded'
-				state.user = action.payload
+				state.status = 'succeeded';
+				state.user = action.payload;
 			})
 			.addCase(login.rejected, (state, action) => {
-				state.status = 'failed'
-				state.error = action.message
-			})
+				state.status = 'failed';
+				state.error = action.message;
+			});
 	},
-})
+});
 
-export const selectUser = (state) => state.auth.user
+export const selectUser = (state) => state.auth.user;
 
-export default authSlice.reducer
+export default authSlice.reducer;
