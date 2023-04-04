77795a9d6813d1b187f70d502acecc0f52a1d305
Jon Green <jongreen1996@gmail.com>
2023-04-04 17:13:46 +0100

    Begun filling page with product content

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,9 +7,29 @@ export default function Product() {
 	const products = useSelector(selectAllProducts);
 	let product = products?.find((product) => product.id === Number(productId));
 
-	return (
-		<div className='page'>
-			<h1 className='font-one'>{product.name}</h1>
-		</div>
-	);
+	if (!product) {
+		return <div className='page'>Product not found</div>;
+	} else {
+		return (
+			<div className='page'>
+				<div className='product'>
+					<div className='product-images tile'>
+						<img src={product.images[0]} alt={product.title} />
+					</div>
+
+					<div className='product-info tile'>
+						<h1 className='font-two'>{product.name}</h1>
+						<p className='font-five'>{product.category}</p>
+						<p className='font-two'>£{product.price}</p>
+						<button className='btn'>Add to basket</button>
+					</div>
+
+					<div className='product-description tile'>
+						<h2 className='font-three'>Description</h2>
+						<p className='font-five'>{product.description}</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
