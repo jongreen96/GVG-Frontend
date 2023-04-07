6f4b9728c8bc62e1e093f0e119b9366fc7d5e0b3
Jon Green <jongreen1996@gmail.com>
2023-04-07 20:46:25 +0100

    Added about/contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,108 @@
+import { useState } from 'react';
+import '../styles/about.css';
+
 export default function About() {
+	const [formComplete, setFormComplete] = useState(false);
+	const [form, setForm] = useState({
+		name: '',
+		email: '',
+		message: '',
+	});
+
+	const handleChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setFormComplete(true);
+		// TODO: Send form data to server
+	};
+
 	return (
 		<div className='page'>
-			<h1>About Page</h1>
+			<h1 className='font-one'>About Us</h1>
+			<div className='tile about-us flex'>
+				<p className='font-five'>
+					Welcome to Green Vinyl Graphics, where we offer the highest
+					quality templates designed to elevate your brand and make
+					your product stand out from the crowd. Our templates are not
+					only perfect for cutting vinyl skins, but they also allow
+					you to create stunning product mock-ups and other unique
+					designs. We pride ourselves on our customer service that is
+					always available to assist you with any question or concern
+					you may have. Our designs are optimized to use with programs
+					such as Cricut, Silhoutte, Cutting Master 4 and Adobe
+					Illustrator making it easy for you to cut your designs with
+					precision and acheive outstanding results. With thousands of
+					satisfied customers, our store has received glowing reviews
+					for our exceptional templates and friendly customer service.
+					We invite you to check out our reviews and see for yourself
+					why our customers love us!
+				</p>
+				<img src='https://i.etsystatic.com/17366700/r/il/0d8bf6/4326201893/il_1588xN.4326201893_itn2.jpg' />
+			</div>
+
+			<h2 className='font-one'>Get in touch</h2>
+			<div className='tile'>
+				<p className='font-five'>
+					Now its easier than ever to get in touch with us and ask us
+					any questions or concerns you may have. Simply fill out the
+					form below and we will get back to you as soon as possible
+					to assist you with anything you need. We value our
+					customers' opinions and want to ensure that your experience
+					with us is top-notch. So don't hesitate to reach out to us
+					and let us know how we can help you.
+				</p>
+				{formComplete ? (
+					<p className='font-five'>
+						Thank you for reaching out to us. We will get back to
+						you as soon as possible. Have a great day!
+					</p>
+				) : (
+					<form
+						className='contact-form'
+						onSubmit={(e) => handleSubmit(e)}
+					>
+						<div className='flex'>
+							<div className='flex-column'>
+								<label htmlFor='name'>Name</label>
+								<input
+									type='text'
+									name='name'
+									placeholder='John Doe'
+									onChange={(e) => handleChange(e)}
+									value={form.name}
+								/>
+							</div>
+
+							<div className='flex-column'>
+								<label htmlFor='email'>Email</label>
+								<input
+									type='email'
+									name='email'
+									placeholder='JohnDoe@example.com'
+									onChange={(e) => handleChange(e)}
+									value={form.email}
+								/>
+							</div>
+						</div>
+
+						<label htmlFor='message'>Message</label>
+						<textarea
+							name='message'
+							rows='4'
+							placeholder='Enter message here'
+							onChange={(e) => handleChange(e)}
+							value={form.message}
+						/>
+
+						<button type='submit' className='btn'>
+							Submit
+						</button>
+					</form>
+				)}
+			</div>
 		</div>
 	);
 }
