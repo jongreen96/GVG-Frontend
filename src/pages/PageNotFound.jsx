9cdc7ff03e359353788d883563cc8c41d6534a79
Jon Green <jongreen1996@gmail.com>
2023-04-08 15:05:02 +0100

    Added return home link to 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function PageNotFound() {
 	return (
 		<div className='page'>
-			<h1>404: Page not found</h1>
+			<h1 className='font-one'>404: Page not found</h1>
+			<Link to='/'>
+				<p className='font-five cta'>Go to Homepage</p>
+			</Link>
 		</div>
 	);
 }
