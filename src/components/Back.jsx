1eee63f11b61e5feb7a800bfa617d14aa5c86033
Jon Green <jongreen1996@gmail.com>
2023-04-20 18:21:14 +0100

    Altered back button for confirmation page

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -1,13 +1,23 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import backIcon from '../assets/icons/back.svg';
 
 export default function Back() {
 	const navigate = useNavigate();
+	const location = useLocation();
 
-	return (
-		<div className='back' onClick={() => navigate(-1)}>
-			<img src={backIcon} alt='back' className='back icon cta' />
-			<p className='back font-four cta'>Back</p>
-		</div>
-	);
+	if (location.pathname === '/confirmation') {
+		return (
+			<div className='back' onClick={() => navigate(-1)}>
+				<img src={backIcon} alt='back' className='back icon cta' />
+				<p className='back font-four cta'>Home</p>
+			</div>
+		);
+	} else {
+		return (
+			<div className='back' onClick={() => navigate(-1)}>
+				<img src={backIcon} alt='back' className='back icon cta' />
+				<p className='back font-four cta'>Back</p>
+			</div>
+		);
+	}
 }
--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -1,13 +1,10 @@
-import { useStripe } from '@stripe/react-stripe-js';
-import { Link } from 'react-router-dom';
+import Back from '../components/Back';
 
 export default function Confirmation() {
 	return (
 		<div className='page'>
+			<Back />
 			<h1 className='font-one'>Confirmation</h1>
-			<Link to='/'>
-				<p className='font-five cta'>Go to Homepage</p>
-			</Link>
 		</div>
 	);
 }
