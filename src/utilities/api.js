540ca23a8e88f1f4ef1cc11e8d0056b4594b5346
Jon Green <jongreen1996@gmail.com>
2023-04-23 15:33:28 +0100

    Added enviroment condition for development

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: '/api/',
+	baseURL:
+		process.env.NODE_ENV === 'production'
+			? 'https://gvg-backend.herokuapp.com/'
+			: 'http://localhost:3000',
 	withCredentials: true,
 });
 
